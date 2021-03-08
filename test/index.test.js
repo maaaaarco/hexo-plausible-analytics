dbbbdfe5be526c33c99ead0eee8289219fec6ad9
Marco <marco@spaghetti.dev>
2021-03-08 16:41:10 +0100

    implemented tests

---
+++ test/index.test.js
@@ -0,0 +1,90 @@
+beforeEach(() => {
+  global.__registerMock = jest.fn();
+  hexo = {
+    extend: {
+      injector: {
+        register: __registerMock,
+      },
+    },
+    config: {},
+  };
+});
+
+test('Does nothing if plugin is disabled', () => {
+  hexo.config.plausible = {
+    enable: false,
+  };
+
+  jest.isolateModules(() => {
+    require('../index.js');
+  });
+
+  expect(__registerMock.mock.calls.length).toBe(0);
+});
+
+test('Throws error if domain is not defined', () => {
+  hexo.config.plausible = {
+    enable: true,
+  };
+
+  expect(() => {
+    jest.isolateModules(() => {
+      require('../index.js');
+    });
+  }).toThrow();
+});
+
+test('Registers specified pages correctly', () => {
+  const pages = ['post', 'page', 'archive'];
+
+  hexo.config.plausible = {
+    enable: true,
+    domain: 'example.com',
+    pages,
+  };
+
+  jest.isolateModules(() => {
+    require('../index.js');
+  });
+
+  expect(__registerMock.mock.calls.length).toBe(3);
+  __registerMock.mock.calls.forEach((call, idx) => {
+    expect(call[0]).toBe('head_end');
+    expect(call[1]).toBeTruthy();
+    expect(call[2]).toBe(pages[idx]);
+  });
+});
+
+test('When pages is undefined registers all pages correctly', () => {
+  hexo.config.plausible = {
+    enable: true,
+    domain: 'example.com',
+  };
+
+  jest.isolateModules(() => {
+    require('../index.js');
+  });
+
+  expect(__registerMock.mock.calls.length).toBe(1);
+  __registerMock.mock.calls.forEach((call) => {
+    expect(call.length).toBe(2);
+  });
+});
+
+test('When pages is set to _default_ registers all pages correctly', () => {
+  hexo.config.plausible = {
+    enable: true,
+    domain: 'example.com',
+  };
+
+  jest.isolateModules(() => {
+    require('../index.js');
+  });
+
+  expect(__registerMock.mock.calls.length).toBe(1);
+  __registerMock.mock.calls.forEach((call) => {
+    expect(call.length).toBe(2);
+  });
+});
+
+
---
+++ test/plausible.test.js
@@ -0,0 +1,108 @@
+const htmlParser = require('node-html-parser');
+
+const DEFAULT_PLAUSIBLE_URL = 'https://plausible.io/js/plausible.js';
+const DEFAULT_PLAUSIBLE_EXCLUSIONS_URL =
+  'https://plausible.io/js/plausible.exclusions.js';
+const DOMAIN = 'example.com';
+
+beforeEach(() => {
+  hexo = {
+    config: {
+      plausible: {
+        enable: true,
+        domain: DOMAIN,
+      },
+    },
+  };
+});
+
+describe('When src is set', () => {
+  test('Ignores other parameters', () => {
+    const src = 'https://custom.url';
+    hexo.config.plausible.subdomain = 'custom';
+    hexo.config.plausible.exclude = ['/test'];
+    hexo.config.plausible.src = src;
+
+    const res = require('../lib/plausible')(hexo)();
+    const scriptElement = htmlParser.parse(res);
+
+    expect(scriptElement.firstChild.getAttribute('src')).toBe(src);
+    expect(scriptElement.firstChild.getAttribute('data-domain')).toBe(DOMAIN);
+  });
+});
+
+describe('When src is not set', () => {
+  describe('When subdomain is not set', () => {
+    test('Loads script from plausible.io', () => {
+      const res = require('../lib/plausible')(hexo)();
+      const scriptElement = htmlParser.parse(res);
+
+      expect(scriptElement.firstChild.getAttribute('src')).toBe(
+        DEFAULT_PLAUSIBLE_URL
+      );
+      expect(scriptElement.firstChild.getAttribute('data-domain')).toBe(DOMAIN);
+    });
+
+    test('Loads exclusions script from plausible.io if exclude is set', () => {
+      hexo.config.plausible.exclude = ['/test'];
+
+      const res = require('../lib/plausible')(hexo)();
+      const scriptElement = htmlParser.parse(res);
+
+      expect(scriptElement.firstChild.getAttribute('src')).toBe(
+        DEFAULT_PLAUSIBLE_EXCLUSIONS_URL
+      );
+      expect(scriptElement.firstChild.getAttribute('data-domain')).toBe(DOMAIN);
+    });
+  });
+
+  describe('When subdomain is set', () => {
+    test('Loads script from subdomain', () => {
+      hexo.config.plausible.subdomain = 'plausible';
+      const res = require('../lib/plausible')(hexo)();
+      const scriptElement = htmlParser.parse(res);
+
+      expect(scriptElement.firstChild.getAttribute('src')).toBe(
+        `https://${hexo.config.plausible.subdomain}.${DOMAIN}/js/index.js`
+      );
+      expect(scriptElement.firstChild.getAttribute('data-domain')).toBe(DOMAIN);
+    });
+
+    test('Loads exclusions script from subdomain if exclude is set', () => {
+      hexo.config.plausible.exclude = ['/test'];
+      hexo.config.plausible.subdomain = 'plausible';
+
+      const res = require('../lib/plausible')(hexo)();
+      const scriptElement = htmlParser.parse(res);
+
+      expect(scriptElement.firstChild.getAttribute('src')).toBe(
+        `https://${hexo.config.plausible.subdomain}.${DOMAIN}/js/index.exclusions.js`
+      );
+      expect(scriptElement.firstChild.getAttribute('data-domain')).toBe(DOMAIN);
+    });
+  });
+
+  describe('When exclude is set', () => {
+    test('Works correctly if value is a comma-separated string', () => {
+      hexo.config.plausible.exclude = '/test, /test2, /test3';
+
+      const res = require('../lib/plausible')(hexo)();
+      const scriptElement = htmlParser.parse(res);
+
+      expect(scriptElement.firstChild.getAttribute('data-exclude')).toBe(
+        hexo.config.plausible.exclude
+      );
+    });
+
+    test('Works correctly if value is a string array', () => {
+      hexo.config.plausible.exclude = ['/test', '/test2', '/test3'];
+
+      const res = require('../lib/plausible')(hexo)();
+      const scriptElement = htmlParser.parse(res);
+
+      expect(scriptElement.firstChild.getAttribute('data-exclude')).toBe(
+        hexo.config.plausible.exclude.join(',')
+      );
+    });
+  });
+});
