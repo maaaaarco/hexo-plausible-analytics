f4e15e560436e094ae5de1028cc0e10e3dd9fff1
Marco <marco@spaghetti.dev>
2021-03-08 16:39:05 +0100

    implemented logic to inject plausible script inside head

---
+++ index.js
@@ -0,0 +1,23 @@
+/* global hexo */
+const injector = require('./lib/plausible');
+const { enable, pages, domain } = hexo.config.plausible;
+
+if (enable) {
+  if (!domain) {
+    throw new Error(
+      '[hexo-plausible-analytics - ERROR]: _domain_ cannot be blank'
+    );
+  }
+
+  if (Array.isArray(pages)) {
+    // registers injector only on specified pages
+    pages.forEach((page) => {
+      hexo.extend.injector.register('head_end', injector(hexo), page);
+    });
+  }
+
+  if (!pages || pages === 'default') {
+    // registers injector on all pages
+    hexo.extend.injector.register('head_end', injector(hexo));
+  }
+}
